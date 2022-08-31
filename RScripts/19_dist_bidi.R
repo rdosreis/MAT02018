831e72239dd6c6ffeff5d1c41f3af54d367a1bc8
rdosreis <rodrigocpdosreis@gmail.com>
2022-08-31 16:41:26 -0300

    Aula 19 - Dist bidimensionais

--- RScripts/19_dist_bidi.R
+++ RScripts/19_dist_bidi.R
@@ -1,4 +1,4 @@
-## ----mb, echo=FALSE, warning=FALSE, message=FALSE---------------------------------------------------------------
+## ----mb, echo=FALSE, warning=FALSE, message=FALSE---------------------------------------------
 
 library(dplyr)
 library(readxl)
@@ -13,7 +13,7 @@ mb_df %>%
 
 
 
-## ----tab_dupla, echo=FALSE, warning=FALSE, message=FALSE--------------------------------------------------------
+## ----tab_dupla, echo=FALSE, warning=FALSE, message=FALSE--------------------------------------
 library(kableExtra)
 
 tab <- mb_df %>% 
@@ -24,12 +24,17 @@ tab <- mb_df %>%
 row.names(tab)[4] <- "Total"
 colnames(tab)[4] <- "Total"
 
-kable(tab, caption = "Distribuição conjunta das frequências das variáveis grau de instrução e região de procedência.") %>% 
+kable(tab,
+      caption = "Distribuição conjunta das frequências das variáveis grau de instrução e região de procedência.", align = "cccc", format = "latex") %>% 
   add_header_above(c("Região de Procedência", "Grau de Instrução" = 3, " "))
 
 
 
-## ----percent_tot_geral, echo=FALSE, warning=FALSE, message=FALSE------------------------------------------------
+
+
+
+
+## ----percent_tot_geral, echo=FALSE, warning=FALSE, message=FALSE------------------------------
 
 prop.tab <- mb_df %>% 
   select(`Região de Procedência`, `Grau de Instrução`) %>% 
@@ -40,12 +45,15 @@ prop.tab <- mb_df %>%
 row.names(prop.tab)[4] <- "Total"
 colnames(prop.tab)[4] <- "Total"
 
-kable(prop.tab, digits = 0, caption = "Distribuição conjunta das frequências relativas (em porcentagem) em relação ao total geral das variáveis grau de instrução e região de procedência.") %>% 
+kable(prop.tab, digits = 0,
+      caption = "Distribuição conjunta das frequências relativas (em porcentagem) em relação ao total geral das variáveis grau de instrução e região de procedência.",
+      align = "cccc",
+      format = "latex") %>% 
   add_header_above(c("Região de Procedência", "Grau de Instrução" = 3, " "))
 
 
 
-## ----percent_tot_coluna, echo=FALSE, warning=FALSE, message=FALSE-----------------------------------------------
+## ----percent_tot_coluna, echo=FALSE, warning=FALSE, message=FALSE-----------------------------
 
 prop.tab <- mb_df %>% 
   select(`Região de Procedência`, `Grau de Instrução`) %>% 
@@ -56,12 +64,16 @@ prop.tab <- mb_df %>%
 row.names(prop.tab)[4] <- "Total"
 colnames(prop.tab)[4] <- "Total"
 
-kable(prop.tab, digits = 0, caption = "Distribuição conjunta das frequências relativas (em porcentagem) em relação aos totais de colunas das variáveis grau de instrução e região de procedência.") %>% 
+kable(prop.tab,
+      digits = 0,
+      caption = "Distribuição conjunta das frequências relativas (em porcentagem) em relação aos totais de colunas das variáveis grau de instrução e região de procedência.",
+      align = "cccc",
+      format = "latex") %>% 
   add_header_above(c("Região de Procedência", "Grau de Instrução" = 3, " "))
 
 
 
-## ----barras, echo=FALSE, warning=FALSE, message=FALSE, fig.align='center', out.width="100%"---------------------
+## ----barras, echo=FALSE, warning=FALSE, message=FALSE, fig.align='center', out.width="90%"----
 
 library(ggplot2)
 library(viridis)
@@ -70,19 +82,31 @@ library(reshape2)
 mb_gg <- mb_df %>% 
   select(`Região de Procedência`, `Grau de Instrução`) %>% 
   table() %>% 
-  prop.table(margin = 2) %>% melt()
+  prop.table(margin = 1) %>% melt()
 
-p <- ggplot(data = mb_gg, mapping = aes(x = `Grau de Instrução`, fill = `Região de Procedência`, y = value)) +
-  geom_bar(position = "fill", stat = "identity") +
+mb_gg <- mb_gg %>%
+  group_by(`Região de Procedência`) %>%
+  mutate(value_pos = cumsum(value) - (0.5 * value)) %>%
+  mutate(value_lab = round(value * 100, 1))
+
+p <- ggplot(data = mb_gg,
+            mapping = aes(x = `Região de Procedência`,
+                          y = value)) +
+  geom_bar(mapping = aes(fill = rev(`Grau de Instrução`)),
+           stat = "identity") +
+  geom_text(mapping = aes(y = value_pos, label = value_lab),
+            col = "white", vjust = 0) +
   scale_fill_viridis(discrete = T) +
   scale_y_continuous(labels = scales::percent) +
-  theme_bw() + ylab("Frequências relativas") +
-  theme(legend.position = "bottom")
+  labs(y = "Frequências relativas", fill = "Grau de Instrução") +
+  theme_bw() + theme(legend.position = "bottom")
 p
 
 
 
-## ----gremio, echo=FALSE, warning=FALSE, message=FALSE-----------------------------------------------------------
+
+
+## ----gremio, echo=FALSE, warning=FALSE, message=FALSE-----------------------------------------
 
 tab <- matrix(c(10, 9, 4, 17, 7, 5),
               byrow = T, ncol = 3, dimnames = list(c("Sem Maicon", "Com Maicon"),
@@ -94,6 +118,8 @@ row.names(prop.tab)[3] <- "Total"
 colnames(prop.tab)[4] <- "Total"
 
 kable(prop.tab, digits = 2,
-      caption = "Resultados dos jogos do Grêmio em 2019.")
+      caption = "Resultados dos jogos do Grêmio em 2019.",
+      align = "cccc",
+      format = "latex", format.args = list(decimal.mark = ","))
 
 
